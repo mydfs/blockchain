84c60f4935eb0e67b0e5f8301e4f97227b7d63fa
Alex Kireev <alexk984@gmail.com>
2018-01-10 14:38:49 +0300

    move game contracts to other branch

--- MyDFS/contracts/interface/BalanceManager.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity ^0.4.16;
-
-interface BalanceManager {
-
-	function deposit(uint32 sum) external; 
-	function depositTo(address to, uint32 sum) external;
-	function withdraw(uint32 sum) external;
-	function balanceOf(uint32 user) public constant returns (uint32);
-}
